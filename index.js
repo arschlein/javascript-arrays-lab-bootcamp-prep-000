8a6bb2962bacf01b1a7d4744032a0dbf9fcfca9a
Monika <arschlein@seznam.cz>
2017-11-05 09:51:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,7 @@ function appendKitten(name){
   var newArray = [...kittens, name];
   return newArray;
 }
+function prependKitten(name){
+  var newArray = [name, ...kittens];
+  return newArray;
+}
