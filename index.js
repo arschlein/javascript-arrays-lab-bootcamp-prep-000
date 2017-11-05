043fed7f9e56fd996cc850a517a8e10f43100e64
Monika <arschlein@seznam.cz>
2017-11-05 09:54:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,9 @@ function prependKitten(name){
   var newArray = [name, ...kittens];
   return newArray;
 }
+function removeLastKitten(){
+  return kittens.slice(0, kittens.lenght -1);
+}
+function removeFirstKitten(){
+  return kittens.slice(0, 1);
+}
